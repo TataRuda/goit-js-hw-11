7d5286a27e7f53306509dcbb0a14df4ddb70e626
Tatsiana Rudava <tatarudova.06@gmail.com>
2023-04-17 21:08:39 +0200

    recommit

---
+++ src/fetchCountries.js
@@ -0,0 +1,6 @@
+export function fetchCountries(name) { 
+// Initializing data fetching
+    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)  
+      .then(response => response.json()) // Response handling
+      .catch(error => console.log(error)) // Error handling
+  } 
