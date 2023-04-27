4006820a20c581efe096565e694f772cceaeb772
Tatsiana Rudava <tatarudova.06@gmail.com>
2023-04-27 21:48:37 +0200

    Update scroll_img.js

--- src/js/scroll_img.js
+++ src/js/scroll_img.js
@@ -3,10 +3,10 @@ export { onScroll }
 window.addEventListener('scroll', onScroll)
 
 function onScroll() {
-const { height: cardHeight } = document.querySelector(".gallery").firstElementChild.getBoundingClientRect();
+const { height } = document.querySelector(".gallery").firstElementChild.getBoundingClientRect();
   
   window.scrollBy({
-    top: cardHeight * 2,
+    top: height * 2,
     behavior: "smooth",
   });
 }
